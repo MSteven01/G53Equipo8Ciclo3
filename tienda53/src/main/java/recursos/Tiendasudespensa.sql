a229035d6336f136a35d2e5d4043293fe6bad749
LuisKrlosHdez <luis.hernandezv@cun.edu.co>
2021-09-28 19:26:52 -0500

    NuevaTablaProveedores

--- tienda53/src/main/java/recursos/Tiendasudespensa.sql
+++ tienda53/src/main/java/recursos/Tiendasudespensa.sql
@@ -18,6 +18,14 @@ CREATE TABLE clientes(
     telefono_cliente VARCHAR (255) NOT NULL
 );
 
+CREATE TABLE proveedores(
+	nit_proveedor BIGINT PRIMARY KEY,
+    nombre_proveedor VARCHAR(255) NOT NULL,
+    direccion_proveedor VARCHAR(255) NOT NULL,
+    telefono_proveedor VARCHAR (255) NOT NULL,
+	ciudad_proveedor VARCHAR (255) NOT NULL
+);
+
 INSERT INTO usuarios VALUES (001,'lolo123@gmail.com','Lolo Perez','admin123456','admininicial');
 INSERT INTO usuarios VALUES (002,'lolo123@gmail.com','Lolo Perez','pass','user');
 INSERT INTO usuarios VALUES (003,'lolo123@gmail.com','Lolo Perez','pass2','user2');
